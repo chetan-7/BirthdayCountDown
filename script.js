2320c7c67de540a8f176b231852b0874b667bb0e
chetan <55053502+chetan-7@users.noreply.github.com>
2021-07-01 16:47:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const Birthday = "2 Jul 2021";
+const Birthday = "23 Oct 2021";
 
 function countdown() {
     const BirthdayDate = new Date(Birthday);
